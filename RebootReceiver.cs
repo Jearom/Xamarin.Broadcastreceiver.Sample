70c00ff07d5e1d477e1906d5e5792dcc5406b45b
jearom <yldrmbaris@hotmail.com>
2020-09-30 01:10:34 +0300

    receiver created

---
+++ RebootReceiver.cs
@@ -0,0 +1,18 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Util;
+using Content = Android.Content;
+
+namespace Xamarin.Broadcastreceiver.Sample
+{
+    [BroadcastReceiver(Enabled = true, Exported = false)]
+    [IntentFilter(new[] { Content.Intent.ActionBootCompleted })]
+    public class RebootReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Log.Debug("Tag", "Receiver tetiklendi.");
+        }
+    }
+}
